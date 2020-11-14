664627fb6bf591791e25d62b36c96b9f8194b330
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2020-11-14 17:23:50 +0300

    Created OpenGL window

--- main.cpp
+++ main.cpp
@@ -7,12 +7,17 @@
 
 #include <iostream>
 #include <windows.h>
+#include "gl/gl.h"
 
-const SIZE MIN_WINDOW_SIZE = SIZE{600, 450};
-const SIZE FIRST_WINDOW_SIZE = SIZE{600, 450};
+const SIZE MIN_WINDOW_SIZE = SIZE{600, 600};
+const SIZE FIRST_WINDOW_SIZE = SIZE{600, 600};
 const COLORREF BACKGROUND_COLOR = RGB(26, 26, 26);
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
+
+void EnableOpenGL(HWND hwnd, HDC *, HGLRC *);
+
+void DisableOpenGL(HWND, HDC, HGLRC);
 
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow) {
@@ -20,9 +25,14 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
     const wchar_t WINDOW_CLASS[] = L"MAIN_WINDOW_CLASS";
     const wchar_t WINDOW_TITLE[] = L"V-3D ^_^";
 
+    HDC hDC;
+    HGLRC hRC;
+    MSG msg;
     WNDCLASSEXW wc;
-    wc.cbSize = sizeof(wc);
-    wc.style = CS_HREDRAW | CS_VREDRAW;
+    HWND hwnd;
+
+    wc.cbSize = sizeof(WNDCLASSEXW);
+    wc.style = CS_OWNDC;
     wc.lpfnWndProc = WindowProc;
     wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
@@ -34,9 +44,12 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
     wc.lpszClassName = WINDOW_CLASS;
     wc.hIconSm = LoadIconW(nullptr, IDI_APPLICATION);
 
-    RegisterClassExW(&wc);
+    if (!RegisterClassExW(&wc)) {
+        MessageBoxW(nullptr, L"Error registering window class", L"Attention", MB_OK);
+        return 0;
+    }
 
-    HWND hwnd = CreateWindowExW(
+    hwnd = CreateWindowExW(
             // Optional window styles.
             0x0,
             // Window class
@@ -64,27 +77,76 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
 
     ShowWindow(hwnd, nCmdShow);
 
-    MSG msg = {};
-    while (GetMessageW(&msg, nullptr, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessageW(&msg);
+    /* enable OpenGL for the window */
+    EnableOpenGL(hwnd, &hDC, &hRC);
+
+
+    bool bQuit = false;
+    float theta = 0.0f;
+    /* program main loop */
+    while (!bQuit) {
+        /* check for messages */
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+            /* handle or dispatch messages */
+            if (msg.message == WM_QUIT) {
+                bQuit = true;
+            } else {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        } else {
+            /* OpenGL animation code goes here */
+
+            glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glPushMatrix();
+            glRotatef(theta, 0.0f, 0.0f, 1.0f);
+
+            glBegin(GL_TRIANGLES);
+
+            glColor3f(1.0f, 0.0f, 0.0f);
+            glVertex2f(0.0f, 1.0f);
+            glColor3f(0.0f, 1.0f, 0.0f);
+            glVertex2f(0.87f, -0.5f);
+            glColor3f(0.0f, 0.0f, 1.0f);
+            glVertex2f(-0.87f, -0.5f);
+
+            glEnd();
+
+            glPopMatrix();
+
+            SwapBuffers(hDC);
+
+            theta += 1.0f;
+            Sleep(1);
+        }
     }
 
-    return 0;
+    /* shutdown OpenGL */
+    DisableOpenGL(hwnd, hDC, hRC);
+
+    /* destroy the window explicitly */
+    DestroyWindow(hwnd);
+
+    return msg.wParam;
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
-        case WM_PAINT: {
-            RECT rcClientRect;
-            GetClientRect(hwnd, &rcClientRect);
-
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hwnd, &ps);
-
-            // Draw
-
-            EndPaint(hwnd, &ps);
+        case WM_CLOSE: {
+            PostQuitMessage(0);
+            break;
+        }
+        case WM_DESTROY: {
+            return 0;
+        }
+        case WM_KEYDOWN: {
+            switch (wParam) {
+                case VK_ESCAPE:
+                    PostQuitMessage(0);
+                    break;
+            }
             break;
         }
         case WM_GETMINMAXINFO: {
@@ -93,13 +155,45 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
             lpMMI->ptMinTrackSize.y = MIN_WINDOW_SIZE.cy;
             break;
         }
-        case WM_DESTROY: {
-            PostQuitMessage(0);
-            break;
-        }
-        default: {
+        default:
             return DefWindowProc(hwnd, uMsg, wParam, lParam);
-        }
     }
+
     return 0;
 }
+
+void EnableOpenGL(HWND hwnd, HDC *hDC, HGLRC *hRC) {
+    PIXELFORMATDESCRIPTOR pfd;
+
+    int iFormat;
+
+    /* get the device context (DC) */
+    *hDC = GetDC(hwnd);
+
+    /* set the pixel format for the DC */
+    ZeroMemory(&pfd, sizeof(pfd));
+
+    pfd.nSize = sizeof(pfd);
+    pfd.nVersion = 1;
+    pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+    pfd.iPixelType = PFD_TYPE_RGBA;
+    pfd.cColorBits = 24;
+    pfd.cDepthBits = 16;
+    pfd.iLayerType = PFD_MAIN_PLANE;
+
+    iFormat = ChoosePixelFormat(*hDC, &pfd);
+
+    SetPixelFormat(*hDC, iFormat, &pfd);
+
+    /* create and enable the render context (RC) */
+    *hRC = wglCreateContext(*hDC);
+
+    wglMakeCurrent(*hDC, *hRC);
+}
+
+void DisableOpenGL(HWND hwnd, HDC hDC, HGLRC hRC) {
+    wglMakeCurrent(nullptr, nullptr);
+    wglDeleteContext(hRC);
+    ReleaseDC(hwnd, hDC);
+}
+
