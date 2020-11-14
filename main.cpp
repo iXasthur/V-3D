a2a5b94388d6846578d06262596d673f3f70c180
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2020-11-14 16:12:15 +0300

    Created first window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,106 @@
-#include <iostream>
+#ifndef _UNICODE
+#define _UNICODE
+#endif
+#ifndef UNICODE
+#define UNICODE
+#endif
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <iostream>
+#include <windows.h>
+#include <gdiplus.h>
+
+const SIZE MIN_WINDOW_SIZE = SIZE{600, 450};
+const SIZE FIRST_WINDOW_SIZE = SIZE{600, 450};
+const COLORREF BACKGROUND_COLOR = RGB(26, 26, 26);
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow) {
+
+    const wchar_t WINDOW_CLASS[] = L"MAIN_WINDOW_CLASS";
+    const wchar_t WINDOW_TITLE[] = L"V-3D ^_^";
+
+    WNDCLASSEXW wc;
+    wc.cbSize = sizeof(wc);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WindowProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = LoadIconW(nullptr, IDI_APPLICATION);
+    wc.hCursor = LoadCursorW(nullptr, IDC_ARROW);
+    wc.hbrBackground = CreateSolidBrush(BACKGROUND_COLOR);
+    wc.lpszMenuName = nullptr;
+    wc.lpszClassName = WINDOW_CLASS;
+    wc.hIconSm = LoadIconW(nullptr, IDI_APPLICATION);
+
+    RegisterClassExW(&wc);
+
+    HWND hwnd = CreateWindowExW(
+            // Optional window styles.
+            0x0,
+            // Window class
+            WINDOW_CLASS,
+            // Window text
+            WINDOW_TITLE,
+            // Window style
+            WS_OVERLAPPEDWINDOW,
+            // Size and position
+            CW_USEDEFAULT, CW_USEDEFAULT, FIRST_WINDOW_SIZE.cx, FIRST_WINDOW_SIZE.cy,
+            // Parent window
+            nullptr,
+            // Menu
+            nullptr,
+            // Instance handle
+            hInstance,
+            // Additional application data
+            nullptr
+    );
+
+    if (hwnd == nullptr) {
+        MessageBoxW(nullptr, L"Error creating window", L"Attention", MB_OK);
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+
+    MSG msg = {};
+    while (GetMessageW(&msg, nullptr, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessageW(&msg);
+    }
+
+    return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_PAINT: {
+            RECT rcClientRect;
+            GetClientRect(hwnd, &rcClientRect);
+
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+
+            // Draw
+
+            EndPaint(hwnd, &ps);
+            break;
+        }
+        case WM_GETMINMAXINFO: {
+            auto lpMMI = (LPMINMAXINFO) lParam;
+            lpMMI->ptMinTrackSize.x = MIN_WINDOW_SIZE.cx;
+            lpMMI->ptMinTrackSize.y = MIN_WINDOW_SIZE.cy;
+            break;
+        }
+        case WM_DESTROY: {
+            PostQuitMessage(0);
+            break;
+        }
+        default: {
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+        }
+    }
     return 0;
 }
